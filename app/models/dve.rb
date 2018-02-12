cef86708fd4caaee84269c9dbd0091cf5d5a1d18
Yuri Tavares <y-tavares@hotmail.com>
2018-02-12 13:41:47 -0200

    some models validations

--- app/models/dve.rb
+++ app/models/dve.rb
@@ -1,2 +1,3 @@
 class Dve < ApplicationRecord
+    validates :event_name, :event_date, :overnight, presence: true
 end
--- spec/models/dve_spec.rb
+++ spec/models/dve_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Dve, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:event_name) }
+    it { is_expected.to validate_presence_of(:event_date) }
+    it { is_expected.to validate_presence_of(:overnight) }
+  end
 end
