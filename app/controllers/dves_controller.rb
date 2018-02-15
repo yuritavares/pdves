f14be43dc86b622b91307a1e6c60ae89b7069958
Yuri Tavares <y-tavares@hotmail.com>
2018-02-15 10:39:46 -0200

    Created action new

--- app/controllers/dves_controller.rb
+++ app/controllers/dves_controller.rb
@@ -1,4 +1,27 @@
 class DvesController < ApplicationController
   def index
   end
+
+  def new
+    @dve = Dve.new
+    @dve.event_date = params[:event_date]
+  end
+
+  def create
+    @dve = Dve.new(dve_params)
+    if @dve.save
+      redirect_to @dve, notice: 'Dve criada com sucesso!'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @dve = Dve.find(params[:id])
+  end
+
+  private
+    def dve_params
+      params.require(:dve).permit(:event_name, :event_date, :start_day, :end_day, :overnight)
+    end
 end
