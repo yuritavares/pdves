6e39f4d548a3513ac56729f6f5c4faff625fd091
Yuri Tavares <y-tavares@hotmail.com>
2018-02-16 07:56:05 -0200

    Before save method to calculate dve values

--- app/models/dve.rb
+++ app/models/dve.rb
@@ -1,3 +1,20 @@
 class Dve < ApplicationRecord
-    validates :event_name, :event_date, presence: true
+    before_save :calculation
+    validates :event_name, :event_date, :day_kind, presence: true
+
+    enum day_kind: %i[Weekend Weekday Holiday]
+
+    def calculation
+        self.total = 0
+        if overnight == true
+            self.total += 20.00
+        end 
+        if day_kind == 'Weekend'
+            self.total += 36.00
+        elsif day_kind == 'Weekday'
+            self.total += 18.00
+        else
+            self.total += 36.00
+        end
+    end
 end
