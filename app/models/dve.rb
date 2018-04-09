b00c28ecb411dac2326f642e3dbbb588ba9d3fb6
Yuri Tavares <y.tavares.info@gmail.com>
2018-04-09 20:21:28 -0300

    Method name changed

--- app/models/dve.rb
+++ app/models/dve.rb
@@ -1,13 +1,13 @@
 class Dve < ApplicationRecord
-  before_save :calculation
-  
+  before_save :day_kind_values
+
   belongs_to :user
-  
+
   validates :event_name, :event_date, :day_kind, presence: true
 
   enum day_kind: %i[Final_de_semana Dia_de_semana Feriado]
 
-  def calculation
+  def day_kind_values
     self.total = 0
     self.total += 20.00 if overnight == true
     self.total += if day_kind == 'Final_de_semana'
